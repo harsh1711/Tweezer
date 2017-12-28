5901eebca0b055b343eda17f8cc892b5e7aa79f5
harsh1711 <34546712+harsh1711@users.noreply.github.com>
2017-12-29 01:13:30 +0530

    Update authentication.R

--- authentication.R
+++ authentication.R
@@ -1,4 +1,8 @@
-#make sure you have installed it first by: 
+## Tweezer ##
+
+## Install the following libraries, if you already have then skip this step.
+## libraries
+
 #install.packages("twitteR") 
 #install.packages("ROAuth")
 
@@ -6,7 +10,7 @@ library(ROAuth)
 library(twitteR)
 
 # access twitter API
-#Insert your consumer key,consumer_secret,access_token,access_secret
+#Replacer your consumer key,consumer_secret,access_token and access_secret
 
 consumer_key <- 'gl3I2S5RuBoZxdy4da0EY'
 consumer_secret <- 'vb645D35mBc4HzGF5onJrFCW1ddw3dhECVzoBhegrzY2D'
@@ -15,3 +19,5 @@ access_secret <- '947HM80CwE1D5mbuhyPYYQgBYCMwd0Q1IVYEH'
 setup_twitter_oauth(consumer_key = consumer_key,
                     consumer_secret = consumer_secret,
                     access_token = access_token, access_secret = access_secret)
+
+# in selection "Using direct authentication" select "2" 
